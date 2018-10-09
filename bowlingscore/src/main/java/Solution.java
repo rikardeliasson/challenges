cbff5abe84f091d442b5ec723f1b1f3301cde236
rikardeliasson <rikard.eliasson8@gmail.com>
2018-10-09 23:05:26 +0200

    playing around with log4j

--- bowlingscore/src/main/java/Solution.java
+++ bowlingscore/src/main/java/Solution.java
@@ -1,5 +1,22 @@
+
+
+
+
+
+import org.apache.log4j.Logger;
+
+import java.util.Arrays;
+
 public class Solution {
+
+    private static final Logger LOGGER = Logger.getLogger(Solution.class);
+
     public static int bowling_score(String frames) {
+
+
+        String[] game = frames.split(" ");
+        Arrays.stream(game).forEach(LOGGER.debug("I am logging that {} happened.", () -> compute()));
+
         return 0;
     }
 }
--- bowlingscore/src/test/java/SolutionTest.java
+++ bowlingscore/src/test/java/SolutionTest.java
@@ -1,11 +1,29 @@
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
     public void BasicTests() {
+
         System.out.println("Maybe this bowler should put bumpers on...\n ");
         assertEquals(20, Solution.bowling_score("11 11 11 11 11 11 11 11 11 11"));
 
         System.out.println("Woah! Perfect game!");
         assertEquals(300, Solution.bowling_score("X X X X X X X X X XXX"));
+    }
+
+    @Test
+    public void shouldReturnReturnZeroIfNotCompleteRound() {
+        assertEquals(0, Solution.bowling_score("11 11 11 11 11 11 11 11 11"));
+    }
+
+    @Test
+    public void shouldReturnZeroIfNoPinsKnockedDown() {
+        assertEquals(0, Solution.bowling_score("00 00 00 00 00 00 00 00 00 00"));
+    }
+
+    @Test
+    public void shouldReturnZeroIfBonusButNoBonusOnLastFrame() {
+        assertEquals(0, Solution.bowling_score("11 22 33 44 55 66 77 88 99 111"));
     }
 }
